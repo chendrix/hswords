c70063ba3f6c516c303651d2d0399719b675d1b3
Chris Hendrix <cah276@cornell.edu>
2015-01-08 01:18:06 -0800

    Made better algorithm?

--- Main.hs
+++ Main.hs
@@ -12,9 +12,30 @@ corpus :: IO (Set.Set String)
 corpus = fmap (Set.fromList . lines) (readFile "short.txt")
 
 max_subwords_in_corpus :: (Set.Set String) -> String
-max_subwords_in_corpus corpus = fst $ List.maximumBy (comparing snd) $ Map.assocs $ map_of_subword_lengths
+max_subwords_in_corpus corpus = maximumKeyOfValue $ map_of_subword_lengths
   where 
     num_subwords_in_corpus :: String -> Int
     num_subwords_in_corpus str = Set.size $ Set.intersection (Set.fromList $ List.subsequences str) corpus
     map_of_subword_lengths :: Map.Map String Int
     map_of_subword_lengths = Map.fromSet num_subwords_in_corpus corpus
+    
+maximumKeyOfValue :: Map.Map String Int -> String
+maximumKeyOfValue m = Map.foldlWithKey' compare_things first_key m
+  where 
+    first_key = unsafeFirstKey m
+    compare_things :: String -> String -> Int -> String
+    compare_things old_key new_key new_value = if (new_value) >= (unsafeLookup old_key m) then new_key else old_key
+    
+unsafeFirstKey :: Map.Map k v -> k
+unsafeFirstKey m = 
+  case Map.keys m of
+    [] -> error "No keys"
+    (k:_) -> k
+
+unsafeLookup :: Ord k => k -> Map.Map k v -> v
+unsafeLookup key m = 
+  case Map.lookup key m of
+    Nothing -> error "Nothing there to lookup"
+    Just v -> v
+  
+-- foldlWithKey' :: (a -> k -> b -> a) -> a -> Map k b -> a
